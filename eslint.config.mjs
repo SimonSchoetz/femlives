addc30de2ad0864e0dda8816de69a4930f7dabda
Simon Schötz <simonsch.tz@gmail.com>
2024-09-09 20:54:45 +0200

    feat: FL-12 set up pre commit hook for linting staged files

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+/**
+ * This is the eslint config for the lint-staged pre-commit hook
+ */
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
+];
