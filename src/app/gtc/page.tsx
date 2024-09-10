6c98b0d211fdda87c0813c6f2f8711952632b664
Simon Schötz <simonsch.tz@gmail.com>
2024-09-10 10:14:00 +0200

    main: set up page routes for gtc and imprint

---
+++ src/app/gtc/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className='text-5xl'>General terms and conditions</h1>
+    </main>
+  );
+}
---
+++ src/app/imprint/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className='text-5xl'>Imprint</h1>
+    </main>
+  );
+}
