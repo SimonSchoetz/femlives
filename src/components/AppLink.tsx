bd51e5933345e53d66aed17f8dcd6bb14fa258b7
Simon Schötz <simonsch.tz@gmail.com>
2024-09-10 17:10:56 +0200

    feat: FL-16 set up some styling for app link

--- src/components/AppLink.tsx
+++ src/components/AppLink.tsx
@@ -30,9 +30,15 @@ const AppLink = ({
 }: Props) => {
   const linkLayout = getLayout(layout);
   const linkStyle = getStyle(style);
-
+  const sharedStyles = 'text-xl transition';
   return (
-    <Link href={href} className={`${linkLayout} ${linkStyle}`} {...props}>
+    <Link
+      href={href}
+      className={`${
+        props.className ?? ''
+      } ${sharedStyles} ${linkLayout} ${linkStyle}`}
+      {...props}
+    >
       {title}
     </Link>
   );
@@ -41,17 +47,24 @@ const AppLink = ({
 export default AppLink;
 
 const getLayout = (layout: AppLinkLayout): string => {
+  const buttonBorderStyles = 'border-2 border-black rounded-lg p-4 font-medium';
+  const buttonHoverStyles =
+    'hover:shadow-lg hover:shadow-fl-primary hover:border-fl-primary';
+
   const layoutMap: Record<AppLinkLayout, string> = {
-    [AppLinkLayout.LINK]: 'TODO',
-    [AppLinkLayout.BUTTON]: 'TODO',
+    [AppLinkLayout.LINK]:
+      'hover:underline font-normal bg-transparent duration-0',
+    [AppLinkLayout.BUTTON]: `${buttonBorderStyles} ${buttonHoverStyles} duration-150`,
   };
-  return layoutMap[layout];
+
+  return layoutMap[layout] || '';
 };
 
 const getStyle = (layout: AppLinkStyle): string => {
   const layoutMap: Record<AppLinkStyle, string> = {
-    [AppLinkStyle.FULL]: 'TODO',
-    [AppLinkStyle.LIGHT]: 'TODO',
+    [AppLinkStyle.FULL]: 'bg-black text-white hover:bg-fl-primary',
+    [AppLinkStyle.LIGHT]:
+      'text-black bg-transparent text-white hover:text-fl-primary ',
   };
-  return layoutMap[layout];
+  return layoutMap[layout] || '';
 };
