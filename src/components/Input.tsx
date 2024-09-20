c6aca59096fe6359c1381946adf13af5283698e0
furmandarya1@gmail.com <furmandarya1@gmail.com>
2024-09-20 11:36:16 +0200

    feature: email-signup-form autoformat quotes

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,28 +1,32 @@
-import { DetailedHTMLProps, InputHTMLAttributes } from "react";
+import { DetailedHTMLProps, InputHTMLAttributes } from 'react';
 
 type InputProps = DetailedHTMLProps<
   InputHTMLAttributes<HTMLInputElement>,
   HTMLInputElement
 > & { label?: string; errorMsg?: string };
 
-const Input = ({ label, errorMsg, ...inputProps }: InputProps) => {
+const Input: React.FC<Readonly<InputProps>> = ({
+  label,
+  errorMsg,
+  ...inputProps
+}) => {
   return (
     <div>
-      <div className="flex flex-col">
+      <div className='flex flex-col'>
         {!!label && (
-          <label htmlFor="email" className="text-lg font-medium">
+          <label htmlFor='email' className='text-lg font-medium'>
             {label}
           </label>
         )}
         <input
           {...inputProps}
-          className="p-2 border border-gray-300 rounded-md text-foreground bg-white 
+          className='p-2 border border-gray-300 rounded-md text-foreground bg-white 
                            focus:ring-2 focus:ring-fl-primary focus:outline-none 
                            transition-transform duration-200 ease-in-out 
-                           hover:scale-105 hover:border-fl-primary"
+                           hover:scale-105 hover:border-fl-primary'
         />
       </div>
-      {!!errorMsg && <p className="text-red-500 text-sm">{errorMsg}</p>}
+      {!!errorMsg && <p className='text-red-500 text-sm'>{errorMsg}</p>}
     </div>
   );
 };
