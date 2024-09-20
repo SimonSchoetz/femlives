b44361f6007fec883c86256472ae31fcf77c8ea0
furmandarya1@gmail.com <furmandarya1@gmail.com>
2024-09-20 11:25:39 +0200

    feature: email-signup-form input component set up

---
+++ src/components/Input.tsx
@@ -0,0 +1,30 @@
+import { DetailedHTMLProps, InputHTMLAttributes } from "react";
+
+type InputProps = DetailedHTMLProps<
+  InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+> & { label?: string; errorMsg?: string };
+
+const Input = ({ label, errorMsg, ...inputProps }: InputProps) => {
+  return (
+    <div>
+      <div className="flex flex-col">
+        {!!label && (
+          <label htmlFor="email" className="text-lg font-medium">
+            {label}
+          </label>
+        )}
+        <input
+          {...inputProps}
+          className="p-2 border border-gray-300 rounded-md text-foreground bg-white 
+                           focus:ring-2 focus:ring-fl-primary focus:outline-none 
+                           transition-transform duration-200 ease-in-out 
+                           hover:scale-105 hover:border-fl-primary"
+        />
+      </div>
+      {!!errorMsg && <p className="text-red-500 text-sm">{errorMsg}</p>}
+    </div>
+  );
+};
+
+export default Input;
