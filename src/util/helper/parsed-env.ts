e2d19cb5f2b38bf22b9ee94ecc0409d48c0ee684
Simon Schötz <simonsch.tz@gmail.com>
2024-09-23 15:38:08 +0200

    feat: fl-44 add resend api key to env.local

--- src/util/helper/parsed-env.ts
+++ src/util/helper/parsed-env.ts
@@ -4,6 +4,7 @@ const envValidator = z.object({
   HOST: z.string().min(1),
   CONVEX_DEPLOYMENT: z.string().min(1),
   NEXT_PUBLIC_CONVEX_URL: z.string().min(1),
+  RESEND_API_KEY: z.string().min(1),
 });
 
 //eslint-disable-next-line no-process-env
