1e5ebd9b6a9d2af43153b0221d823f166a597127
Simon Schötz <simonsch.tz@gmail.com>
2024-09-24 10:58:00 +0200

    feat: fl-44 fix submit action

--- src/actions/form-submits/submit-email-sign-up.ts
+++ src/actions/form-submits/submit-email-sign-up.ts
@@ -1,9 +1,11 @@
+'use server';
+
 import { createEmailAddress } from '@/api/db/email-addresses';
-import { zAppEmailAddressData } from '@/api/db/validators';
+import { zEmailAddressDto } from '@/api/db/validators';
 import { sendEmailSignUpVerificationEmail } from '../email';
 
 export const submitEmailSignUp = async (data: unknown): Promise<void> => {
-  const { email } = zAppEmailAddressData.parse(data);
+  const { email } = zEmailAddressDto.parse(data);
   try {
     await createEmailAddress(email);
     await sendEmailSignUpVerificationEmail(email);
