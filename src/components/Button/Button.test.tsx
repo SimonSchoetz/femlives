d8e2a7ed63bcf68aae6ed4965a3a0becf7f19df3
furmandarya1@gmail.com <furmandarya1@gmail.com>
2024-09-24 15:59:20 +0200

    feat: fl-55 all tests are passed successfully

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,38 @@
+import '@testing-library/jest-dom';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Button from './Button';
+
+describe('Button component', () => {
+  it('Should not be clickable when disabled', () => {
+    const handleClick = jest.fn();
+
+    render(
+      <Button buttonLabel='Click Me' onClick={handleClick} disabled={true} />
+    );
+
+    const buttonElement = screen.getByText('Click Me');
+
+    expect(buttonElement).toBeDisabled();
+
+    fireEvent.click(buttonElement);
+
+    expect(handleClick).not.toHaveBeenCalled();
+  });
+
+  it('Should display loading state and be unclickable', () => {
+    const handleClick = jest.fn();
+
+    render(
+      <Button buttonLabel='Click Me' onClick={handleClick} loading={true} />
+    );
+
+    const buttonElement = screen.getByText('Loading...');
+
+    expect(buttonElement).toBeDisabled();
+    expect(buttonElement).toHaveTextContent('Loading...');
+
+    fireEvent.click(buttonElement);
+
+    expect(handleClick).not.toHaveBeenCalled();
+  });
+});
