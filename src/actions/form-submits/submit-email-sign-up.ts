99d470472db2577e144481891a1c76f31183651e
Simon Schötz <simonsch.tz@gmail.com>
2024-09-24 14:41:36 +0200

    feat: fl-44 detailed error handling on submit action

--- src/actions/form-submits/submit-email-sign-up.ts
+++ src/actions/form-submits/submit-email-sign-up.ts
@@ -3,16 +3,35 @@
 import { createEmailAddress } from '@/api/db/email-addresses';
 import { zEmailAddressDto } from '@/api/db/validators';
 import { sendEmailSignUpVerificationEmail } from '../email';
+import { ZodError } from 'zod';
+import { FormSubmitResponse } from '@/types/app';
+import { HttpStatusCode } from '@/enums';
 
-export const submitEmailSignUp = async (data: unknown): Promise<void> => {
-  const { email } = zEmailAddressDto.parse(data);
+export const submitEmailSignUp = async (
+  data: unknown
+): Promise<FormSubmitResponse> => {
   try {
+    const { email } = zEmailAddressDto.parse(data);
     await createEmailAddress(email);
     await sendEmailSignUpVerificationEmail(email);
+    return {
+      status: HttpStatusCode.CREATED,
+    };
   } catch (error) {
+    if (error instanceof ZodError) {
+      throw new Error('Could not validate input data');
+    }
     if (error instanceof Error) {
+      if (error.message.includes('Email must be unique')) {
+        return {
+          status: HttpStatusCode.CONFLICT,
+          error: {
+            email: 'Email already in use.',
+          },
+        };
+      }
       throw new Error(error.message);
     }
-    throw error;
+    throw new Error(`Unknown error during sign up: "${error}"`);
   }
 };
