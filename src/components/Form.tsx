d9faecd7e396ec595eb5b67ae726dafdc8d3e40d
furmandarya1@gmail.com <furmandarya1@gmail.com>
2024-09-26 16:19:24 +0200

    feat: fl-56 created form component

---
+++ src/components/Form.tsx
@@ -0,0 +1,90 @@
+'use client';
+
+import React, { ReactElement } from 'react';
+import {
+  useForm,
+  SubmitHandler,
+  FieldValues,
+  Path,
+  FieldErrors,
+} from 'react-hook-form';
+import { ZodSchema } from 'zod';
+import { zodResolver } from '../../node_modules/@hookform/resolvers/zod/src/';
+import Button from './Button/Button';
+
+interface InputProps {
+  name: string;
+  errorMsg?: string;
+}
+
+interface FormProps<T extends FieldValues> {
+  onSubmit: (data: T) => Promise<{ [key in keyof T]?: string } | null>;
+  schema: ZodSchema<T>;
+  children: ReactElement<InputProps>[];
+  submitButtonLabel: string;
+}
+
+const getErrorMessage = <T extends FieldValues>(
+  errors: FieldErrors<T>,
+  fieldName: Path<T>
+): string | undefined => {
+  const error = errors[fieldName]?.message;
+
+  if (typeof error === 'string') {
+    return error;
+  }
+
+  return undefined;
+};
+const Form = <T extends FieldValues>({
+  onSubmit,
+  schema,
+  children,
+  submitButtonLabel,
+}: FormProps<T>) => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+    setError,
+  } = useForm<T>({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmitHandler: SubmitHandler<T> = async (data) => {
+    const result = await onSubmit(data);
+
+    if (result) {
+      (Object.keys(result) as Array<keyof T>).forEach((field) => {
+        setError(field as Path<T>, {
+          type: 'manual',
+          message: result[field] as string,
+        });
+      });
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmitHandler)} className='space-y-4'>
+      {React.Children.map(children, (child) => {
+        if (React.isValidElement<InputProps>(child)) {
+          const fieldName = child.props.name as Path<T>;
+
+          return React.cloneElement(child, {
+            ...child.props,
+            ...register(fieldName),
+            errorMsg: getErrorMessage(errors, fieldName),
+          });
+        }
+        return child;
+      })}
+      <Button
+        type='submit'
+        buttonLabel={submitButtonLabel}
+        loading={isSubmitting}
+      />
+    </form>
+  );
+};
+
+export default Form;
