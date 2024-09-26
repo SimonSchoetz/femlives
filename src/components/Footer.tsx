dd438e63d4d8848e81283adb94777d17560d3744
Simon Schötz <simonsch.tz@gmail.com>
2024-09-26 10:41:34 +0200

    feat: correct-use-of-app-link adjust footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,11 @@
-import { composeUrl } from '@/util/app';
 import AppLink from './AppLink';
 import { Route } from '@/enums';
 
 const Footer = () => {
   return (
     <div className='flex gap-10 h-56 justify-center items-center'>
-      <AppLink title={'Terms of Service'} href={composeUrl(Route.GTC)} />
-      <AppLink title={'Imprint'} href={composeUrl(Route.IMPRINT)} />
+      <AppLink label={'Terms of Service'} internalRoute={Route.GTC} />
+      <AppLink label={'Imprint'} internalRoute={Route.IMPRINT} />
     </div>
   );
 };
